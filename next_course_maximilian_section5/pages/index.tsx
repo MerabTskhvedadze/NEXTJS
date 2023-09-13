df312fcf51419357cd2243c480bd228f0e2ec63c
MerabTskhvedadze <merabi.tskhvedadze@gmail.com>
2023-09-13 12:07:16 +0400

    getting static props lsn94

--- next_course_maximilian_section5/pages/index.tsx
+++ next_course_maximilian_section5/pages/index.tsx
@@ -5,7 +5,9 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home(props: any) {
+  console.log(props);
+
   return (
     <ul>
       <li>Product 1</li>
@@ -13,4 +15,10 @@ export default function Home() {
       <li>Product 3</li>
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: { product: { id: 1, title: "Product 1" } },
+  };
 }
